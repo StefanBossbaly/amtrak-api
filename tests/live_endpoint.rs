4972d127dabd3ad598744f43288050b77ea1e191
Stefan Bossbaly <sbossb@gmail.com>
2024-05-09 21:09:36 -0400

    Added live endpoint tests
    Will setup a chron job that will test the live endpoint. That way if it
    fails I will get an email.

---
+++ tests/live_endpoint.rs
@@ -0,0 +1,25 @@
+use amtrak_api::Client;
+
+#[tokio::test]
+async fn test_live_train_api() -> Result<(), amtrak_api::errors::Error> {
+    let client = Client::new();
+    let response = client.trains().await?;
+
+    for (train_num, _) in response.0.iter() {
+        client.train(train_num).await?;
+    }
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_live_station_api() -> Result<(), amtrak_api::errors::Error> {
+    let client = Client::new();
+    let response = client.stations().await?;
+
+    for (station_code, _) in response.0.iter() {
+        client.station(station_code).await?;
+    }
+
+    Ok(())
+}
